014f90f6c3b1bd0a33cabb33f915dc1939d7cbb0
Mohnish <mohnish.gorasia@gmail.com>
2013-02-26 20:27:29 -0500

    date made dynamic

--- httpTemplate.py
+++ httpTemplate.py
@@ -1,4 +1,4 @@
-
+import datetime
 import jinja2
 
 # NOTE: in this template there is no data relating to specific posts.
@@ -11,8 +11,8 @@ page_template = jinja2.Template('''
         
         <h1><font color="blue"> Welcome to Freeseer online portal</font></h1>
         {% for post in posts %}
-          <li><a href="{{ post.url }}">{{ post.title }} 
-                                       -{{post.content}}</a></li>
+          <li><a href="{{ post.url }}">{{post.title}}-{{post.content}} 
+                                       </a></li>
           
           
           <iframe src="{{post.url}}" width="500" height="200"></iframe>
@@ -35,44 +35,49 @@ page_template = jinja2.Template('''
 
 # NOTE your main code would create a data structure such as this 
 # list of dictionaries ready to pass in to your template
+now=datetime.date.today()
+
 list_of_posts = [
          { 'url' : 'http://www.icecast.org/',
           'title' : 'Icecast',
-          'date' : 'Feb 2013',
+          'date' : now,
           'content' : 'This is the icecast streaming channel'},
 
          { 'url' : 'http://www.irc.org/',
           'title' : 'IRC channel',
-          'date' : 'Feb 2013',
+          'date' : now,
           'content' : 'This is the IRC channel'},
          
          {'url': 'http://etherpad.org/',
           'title':'Etherpad',
-          'date':'Feb 2013',
+          'date':now,
           'content':'Etherpad is an online editor'},
          ]
 
 
 # Pass in a full list of posts and a variable containing the last
 # post in the list, assumed to be the most recent. 
-# s=page_template.render(posts = list_of_posts,
-#                            current = list_of_posts[0])
-# with open("httpTemplate.html","w") as html_file:
-#     html_file.write(s)
-#     
-# f2=open("httpTemplate.html","r+")
-# m=f2.read()
-# print m
+s=page_template.render(posts = list_of_posts,
+                            current = list_of_posts[0])
+with open("httpTemplate.html","w") as html_file:
+    html_file.write(s)
+     
+f2=open("httpTemplate.html","r+")
+m=f2.read()
+print m
 
 
 # pass the event with url data and location to store the resulting html file
 # apply html template and store it to file 
-def createHTMLFile(event, fileLocation):
-    eventPost = event.getHtmlFormat()
-    s=page_template.render(posts = eventPost,
-                           current = eventPost[0])
-    with open(fileLocation,"w") as html_file:
-        html_file.write(s)
+#def createHTMLFile(event, fileLocation):
+#    eventPost = event.getHtmlFormat()
+#    s=page_template.render(posts = eventPost,
+#                           current = eventPost[0])
+#    with open(fileLocation,"w") as html_file:
+#        html_file.write(s)
+        
+
+
     # f2=open("httpTemplate.html","r+")
     # m=f2.read()
     # print m
